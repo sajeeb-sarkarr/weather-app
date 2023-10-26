3e3d8f802f3be4dd884f0b9f6658291ebc944c2c
Sajeeb Sarkar <148118432+sajeeb-sarkarr@users.noreply.github.com>
2023-10-26 18:48:58 +0000

    timer made for showing current time

---
+++ script.js
@@ -0,0 +1,11 @@
+setInterval(clock, 1000);
+function clock() {
+  let time = new Date();
+  let hour = time.getHours();
+  let min = time.getMinutes();
+
+  let currentTime = hour + ":" + min
+
+  document.getElementById("time").innerHTML = currentTime;
+}
+clock();
